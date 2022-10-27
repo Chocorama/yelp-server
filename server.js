4755e7f0f64cf49e650155f42cf2861efff81d55
Willem Humphrey <willemhumphrey@gmail.com>
2022-10-27 09:09:33 -0500

    add two routes (all restaurants and a singular restaurant)

--- server.js
+++ server.js
@@ -3,7 +3,25 @@ const express = require("express");
 
 const app = express();
 
-app.get("/getRestaurants", (req, res) => {});
+// get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: ["Mcdonalds, 'Wendys"],
+    },
+  });
+});
+
+// get a restaurant
+// params: { id: '1234' }
+app.get("/api/v1/restaurants/:restaurantid", (req, res) => {
+  const {
+    params: { restaurantid },
+  } = req;
+
+  console.log(restaurantid);
+});
 
 const port = process.env.PORT || 3000;
 
